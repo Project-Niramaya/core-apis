08b2f8a4003e5e4058f0ad4f1e76699fd8255c6b
Tejas <tejasrc27@gmail.com>
2023-10-13 19:41:44 +0530

    APIs for ABHA registration with added comments

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from pydantic import BaseModel
 
 import requests
 
-app = FastAPI()
+app = FastAPI()                 #instantiate the FastAPI app
+
+
+#following are pydantic data model classes used for data validation and basic data structure used for data in transit in APIs
 
 class HealthId(BaseModel):
     healthId : str
@@ -91,7 +94,7 @@ def existsByHealthId(healthId : HealthId):
 
 
 
-# Following are API endoints used in registering a new user
+# Following are API endoints for basic apis provided by ABHA sandbox for registration
 @app.post("/generateOtp")
 def generateOtp(aadhaar : Aadhaar):
     url = "https://healthidsbx.abdm.gov.in/api/v1/registration/aadhaar/generateOtp"
@@ -145,36 +148,38 @@ def createHealthIdWithPreVerified(details : Details):
     response = sendHTTPRequest(url, data)
     return response
 
-    
-@app.post("/register")
-def registerNewUser(regDetails : RegistrationDetails):
-    aadhaarData = {"aadhaar" : regDetails.aadhaar}
-    a1 = Aadhaar(**aadhaarData)
-    generateOtpResponse = generateOtp(a1)
-    txnId = generateOtpResponse["txnId"]
-    return {"txnId" : txnId, "mobile" : regDetails.mobile}
 
-@app.post("/submitOtp")
+#following api endpoints are hit to register new user with ABHA from the frontend. All initial data populated from forms. 
+   
+@app.post("/register")
+def registerNewUser(aadhaar : str = Form(...), mobile : str = Form(...)): #takes the aadhaar and mobile no from the form
+    aadhaarData = {"aadhaar" : aadhaar}
+    a1 = Aadhaar(**aadhaarData)
+    generateOtpResponse = generateOtp(a1)   #bind the data in a pydantic data model object to call generateOtp api to start a new transaction
+    txnId = generateOtpResponse["txnId"]
+    return {"txnId" : txnId, "mobile" : mobile}     #return the transaction id to the frontend
+
+@app.post("/submitOtp")         #obtain otp received as sms from the frontend
 def submitOtp(mobile : str, txnId : str, otp : str = Form(...)):
     otpData = {"otp" : otp, "txnId" : txnId}
-    t1 = Transaction(**otpData)
+    t1 = Transaction(**otpData)         #bind the otp and the transaction id as a pydantic data model object to call verifyOtp
     verifyOtpResponse = verifyOtp(t1)
-    txnId = verifyOtpResponse["txnId"]
-    mobileOtpData = {"mobile" : mobile, "txnId" : txnId}
+    txnId = verifyOtpResponse["txnId"]      #transaction id obtained again
+    mobileOtpData = {"mobile" : mobile, "txnId" : txnId}    #bind the txnId and the mobile no as a pydantic data object to call generateMobileOtp
     m1 = MobileOTPTransaction(**mobileOtpData)
     generateMobileOtpResponse = generateMobileOTP(m1)
     txnId = generateMobileOtpResponse["txnId"]
-    return {"txnId" : txnId}
+    return {"txnId" : txnId}                    #return the txnId obtained to the frontend
 
 @app.post("/verifySecondOtp")
-def verifySecondOtp(txnId : str, otp : str = Form(...)):
+def verifySecondOtp(txnId : str, otp : str = Form(...)):    #get the second otp received as sms from the frontend form
     otpVerificationData = {"otp" : otp, "txnId" : txnId}
-    v1 = Transaction(**otpVerificationData)
+    v1 = Transaction(**otpVerificationData)                 #bind the otp and txnId as pydantic data object to call verifyMobileOtp api
     verifyMobileOtpResponse = verifyMobileOTP(v1)
     txnId = verifyMobileOtpResponse["txnId"]
-    return {"txnId" : txnId}
+    return {"txnId" : txnId}                        #return the txnId obtained as response to the frontend
 
-@app.post("/submitRegDetails")
+@app.post("/submitRegDetails")                      #obtain all the basic details of the user for registration from the frontend form
 def submitRegDetails(txnId : str,
                     email : str = Form(...), 
                     firstName : str = Form(...),
@@ -184,20 +189,21 @@ def submitRegDetails(txnId : str,
                     password : str = Form(...), 
                     profilePhoto : str = Form(...), 
                     ):
+    
     createHealthIdData = {
-                            "email" : email, 
+                            "email" : email,                    #bind the registration details obtained from the frontend as a pydantic data object
                             "firstname" : firstName,
                             "healthId" : healthId, 
                             "lastname" : lastName, 
                             "middleName" : middleName, 
                             "password" : password, 
                             "profilePhoto" : profilePhoto, 
-                            "txnId" : txnId
+                            "txnId" : txnId                     #txnId returned in the previous api must also be bound in the data object
                         }
     
-    d1 = Details(**createHealthIdData)
-    createHealthIdResponse= createHealthIdWithPreVerified(d1)
-    return createHealthIdResponse
+    d1 = Details(**createHealthIdData)                          #create pydantic model object
+    createHealthIdResponse= createHealthIdWithPreVerified(d1)   #make a call to register the user with ABHA
+    return createHealthIdResponse                               #return the response to the frontend for display
     
 
 
